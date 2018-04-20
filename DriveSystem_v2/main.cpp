6c8f13c76a38b672f6654e6b93ed0fa87de6d202
Spear <ofrim@ualberta.ca>
2018-04-20 11:13:32 -0600

    Adds autogenerated template files

---
+++ DriveSystem_v2/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * DriveSystem_v2.cpp
+ *
+ * Created: 2018-04-20 11:07:20 AM
+ * Author : spear
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
