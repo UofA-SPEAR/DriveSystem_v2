9a63c4c3c35cdc89b416ce0c33b02f6f299a7979
Spear <ofrim@ualberta.ca>
2018-04-20 13:48:50 -0600

    Copy timer files from old drive system

--- DriveSystem_v2/Timer/timer.c
+++ DriveSystem_v2/Timer/timer.c
@@ -4,3 +4,28 @@
  * Created: 2018-04-20 1:46:18 PM
  *  Author: spear
  */ 
+
+
+#include "timer.h"
+
+void setup_timer()
+{
+	TCCR3B |= _BV(WGM32); // configure timer3 for CTC mode
+	TIMSK3 |= _BV(OCIE3A); // enable the CTC interrupt b
+	TCCR3B |= T3_PRESCALE_0;
+	OCR3A = COUNTER_MAX_16;
+	
+	TCCR1A |= _BV(COM1A1) | _BV(COM1B1); // set none-inverting mode
+	TCCR1A |= _BV(WGM10); // set PWN Phase Corrected
+	TCCR1B |= T1_PRESCALE_32;
+	
+	OCR1A = 0; // set the CTC compare value = left motor
+	OCR1B = 0; // set the CTC compare value = right motor
+}
+
+void WDT_interrupt_enable(void)
+{
+	/* Timed sequence writing WDCE and WDE. See data sheet chapter 13*/
+	WDTCSR |= (1<<WDCE)|(1<<WDE);				//Change enabled
+	WDTCSR = (1<< WDIE)|(1<<WDP2)|(1<<WDP0);	//Configure interrupt with 1 sec timeout
+}
--- DriveSystem_v2/Timer/timer.h
+++ DriveSystem_v2/Timer/timer.h
@@ -10,7 +10,28 @@
 #define TIMER_H_
 
 
+#define TIMER_PRESCALE 10
+#define COUNTER_MAX_16 65535
+#define WDTO_500MS 5
 
+#define T1_PRESCALE_0		0x00 | _BV(CS10)
+#define T1_PRESCALE_8		0x00 | _BV(CS11)
+#define T1_PRESCALE_32		0x00 | _BV(CS11) | _BV(CS10)
+#define T1_PRESCALE_64		0x00 | _BV(CS12)
+#define T1_PRESCALE_128		0x00 | _BV(CS12) | _BV(CS10)
+#define T1_PRESCALE_256		0x00 | _BV(CS12) | _BV(CS11)
+#define T1_PRESCALE_1024	0x00 | _BV(CS12) | _BV(CS11) | _BV(CS10)
+
+#define T3_PRESCALE_0		0x00 | _BV(CS30)
+#define T3_PRESCALE_8		0x00 | _BV(CS31)
+#define T3_PRESCALE_32		0x00 | _BV(CS31) | _BV(CS30)
+#define T3_PRESCALE_64		0x00 | _BV(CS32)
+#define T3_PRESCALE_128		0x00 | _BV(CS32) | _BV(CS30)
+#define T3_PRESCALE_256		0x00 | _BV(CS32) | _BV(CS31)
+#define T3_PRESCALE_1024	0x00 | _BV(CS32) | _BV(CS31) | _BV(CS30)
+
+void setup_timer();
+void WDT_interrupt_enable(void);
 
 
 #endif /* TIMER_H_ */
