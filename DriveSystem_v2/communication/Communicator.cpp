b5357c3c47181c155fc30499aa61ce8320fe59c1
Spear <ofrim@ualberta.ca>
2018-04-20 15:44:48 -0600

    Adds the Communicator implementations

--- DriveSystem_v2/communication/Communicator.cpp
+++ DriveSystem_v2/communication/Communicator.cpp
@@ -4,3 +4,16 @@
  * Defines communication interface API.
  * Author: Brad Ofrim
  */ 
+
+#include "Communicator.h"
+
+/* 
+ * Name: Communicator Constructor
+ * Description: Constructor for the Communicator class.
+ * Inputs: None
+ * Outputs: None
+*/
+Communicator::Communicator(void)
+{
+	message_ = NULL;	
+};
--- DriveSystem_v2/communication/Communicator.h
+++ DriveSystem_v2/communication/Communicator.h
@@ -10,7 +10,17 @@
 #define COMMUNICATOR_H_
 
 
-
+class Communicator
+{
+protected:
+	char* message_;
+public:
+	Communicator(void);
+	virtual bool SendMessage(const char* send_message) = 0;
+	virtual bool HasPendingMessage(void) = 0;
+	virtual const char* GetMessage(void) = 0;
+	virtual const char* GetPendingMessage(void) = 0;
+};
 
 
 #endif /* COMMUNICATOR_H_ */
