19816af319b784b9d48f6ca853929d9785018fe0
Spear <ofrim@ualberta.ca>
2018-04-20 13:53:59 -0600

    Copy uart files from old drive system

--- DriveSystem_v2/UART/uart.h
+++ DriveSystem_v2/UART/uart.h
@@ -10,7 +10,24 @@
 #define UART_H_
 
 
+#include <stdio.h>
+#include "hardware_config.h"
 
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+#ifndef BAUD
+#define BAUD 9600
+#endif
+
+FILE uart_output;
+FILE uart_input;
+
+void uart_init();
+void uart_set_io_streams();
+void uart_putchar(char c, FILE *stream);
+char uart_getchar(FILE *stream);
 
 
 #endif /* UART_H_ */
